e5025e2e1213df93412be1872f61616e1d0ad968
Ondřej Garncarz <ondrej@garncarz.cz>
2018-01-02 03:24:27 +0100

    Replica of master server.

---
+++ nogamespy/protocol.py
@@ -0,0 +1,42 @@
+import logging
+import socket
+import socketserver
+import struct
+
+import aluigi
+from . import models, settings
+
+
+logger = logging.getLogger(__name__)
+
+
+def _encode_list(servers):
+    return aluigi.encodeList('bq98mE', memoryview(servers).tobytes())
+
+
+class MasterService(socketserver.TCPServer):
+
+    allow_reuse_address = True
+
+    def __init__(self):
+        super().__init__(('', settings.MASTER_PORT), MasterHandler)
+
+
+class MasterHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        logger.debug(f'Responding to {self.request.getpeername()[0]}...')
+
+        self.request.sendall('\\basic\\\\secure\\'.encode('latin1'))
+        line1 = self.request.recv(4096).decode('latin1')
+        line2 = self.request.recv(4096).decode('latin1')
+
+        # TODO cache for some small amount of time
+        servers = bytearray()
+        for server in models.Server.query.filter_by(online=True).all():
+            servers.extend(socket.inet_aton(server.ip))
+            servers.extend(struct.pack('>h', server.info_port))
+        servers.append(0x5c)
+        servers.append(0x66)
+
+        self.request.send(_encode_list(servers))
--- nogamespy/settings.py
+++ nogamespy/settings.py
@@ -2,6 +2,8 @@ DATABASE = 'postgresql://localhost/vietcong'
 
 UDP_TIMEOUT = 4
 
+MASTER_PORT = 28900
+
 try:
     from .settings_local import *
 except ImportError:
