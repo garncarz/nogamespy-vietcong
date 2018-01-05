a6e43c5ec07e94a80a388ca5cb4d7b1a17dd4985
Ondřej Garncarz <ondrej@garncarz.cz>
2018-01-05 04:30:29 +0100

    Celery integrated.

--- nogamespy/settings.py
+++ nogamespy/settings.py
@@ -1,3 +1,5 @@
+from celery.schedules import crontab
+
 DATABASE = 'sqlite:///volume/db.sqlite'
 
 GAMESPY_KEY = 'bq98mE'
@@ -6,6 +8,23 @@ UDP_TIMEOUT = 4
 
 MASTER_PORT = 28900
 HEARTBEAT_PORT = 27900
+
+# Celery:
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+BROKER_URL = 'redis://redis'
+
+CELERYBEAT_SCHEDULE = {
+    'refresh_all_servers': {
+        'task': 'nogamespy.tasks.refresh_all_servers',
+        'schedule': crontab(minute='*/2'),
+    },
+    'pull_master': {
+        'task': 'nogamespy.tasks.pull_master',
+        'schedule': crontab(minute='*/5'),
+    },
+}
 
 try:
     from .settings_local import *
