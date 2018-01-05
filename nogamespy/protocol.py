88b39c6434cb87ae3e2c10259000cf8cd9fcd898
Ondřej Garncarz <ondrej@garncarz.cz>
2018-01-05 05:24:01 +0100

    Refactor: two function moved to protocol.py.

--- nogamespy/protocol.py
+++ nogamespy/protocol.py
@@ -1,4 +1,5 @@
 import logging
+import re
 import socket
 import socketserver
 import struct
@@ -73,3 +74,35 @@ class HeartbeatHandler(socketserver.BaseRequestHandler):
             return
 
         tasks.register(ip=self.client_address[0], port=msg[2])
+
+
+def fetch_from_master(ip):
+    logger.debug(f'Fetching new servers from {ip}...')
+
+    client = socket.create_connection((ip, settings.MASTER_PORT))
+
+    client.recv(4096)
+
+    client.send(b'\\gamename\\vietcong\\gamever\\2\\location\\0\\validate\\JOzySo8c\\enctype\\2\\final\\'
+                b'\\queryid\\1.1\\\\list\\cmp\\gamename\\vietcong\\final\\')
+
+    resp = client.recv(4096)
+    servers = decode_list(resp)
+
+    client.close()
+
+    return servers
+
+
+def get_server_info(server):
+    logger.debug(f'Trying to get info from {server.ip}:{server.info_port}...')
+
+    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp.settimeout(settings.UDP_TIMEOUT)
+    udp.connect((server.ip, int(server.info_port)))
+
+    udp.send('\\status\\players\\'.encode('ascii'))
+    data = udp.recv(4096).decode('ascii')
+    arr = re.split('\\\\', data)[1:-4]
+
+    return dict(zip(arr[::2], arr[1::2]))
