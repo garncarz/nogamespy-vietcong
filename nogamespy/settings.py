8325b34f38fef4c6ed6c4e6ded2d622b65cf038a
Ondřej Garncarz <ondrej@garncarz.cz>
2018-01-06 01:58:26 +0100

    Sentry/Raven integrated.
    Own logging configuration.

--- nogamespy/settings.py
+++ nogamespy/settings.py
@@ -1,3 +1,5 @@
+import logging.config
+
 from celery.schedules import crontab
 
 DATABASE = 'sqlite:///volume/db.sqlite'
@@ -14,6 +16,7 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 BROKER_URL = 'redis://redis'
+CELERYD_HIJACK_ROOT_LOGGER = False
 
 CELERYBEAT_SCHEDULE = {
     'refresh_all_servers': {
@@ -26,11 +29,70 @@ CELERYBEAT_SCHEDULE = {
     },
 }
 
+SENTRY_DSN = None
+
 STATSD_HOST = 'localhost'
 STATSD_PORT = 8125
 STATSD_PREFIX = 'nogamespy'
+
+
+def sentry_filter(record):
+    if (record.name.startswith('celery.')
+            and record.levelno < logging.WARNING):
+        return False
+
+    if record.levelno < logging.INFO:
+        return False
+
+    return True
+
+
+LOGGING = lambda: {
+    'version': 1,
+    'disable_existing_loggers': True,
+
+    'formatters': {
+        'verbose': {
+            'format': '[%(asctime)s][%(levelname)s] %(name)s '
+                      '%(filename)s:%(funcName)s:%(lineno)d | %(message)s',
+        },
+    },
+
+    'filters': {
+        'mute_at_sentry': {
+            '()': lambda: sentry_filter,
+        },
+    },
+
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+        'sentry': {
+            'class': 'raven.handlers.logging.SentryHandler',
+            'dsn': SENTRY_DSN,
+            'filters': ['mute_at_sentry'],
+            # 'release': raven.fetch_git_sha(BASE_DIR),
+        },
+    },
+
+    'loggers': {
+        '': {
+            'handlers': ['console', 'sentry'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
+
 
 try:
     from .settings_local import *
 except ImportError:
     pass
+
+
+LOGGING = LOGGING()
+logging.config.dictConfig(LOGGING)
