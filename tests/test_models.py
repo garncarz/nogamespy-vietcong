305e5821c32c72f1d1cc86657807d7e33e74d3f8
Ondřej Garncarz <ondrej@garncarz.cz>
2018-01-06 02:48:50 +0100

    Models: more relationships, __repr__s.

--- tests/test_models.py
+++ tests/test_models.py
@@ -17,18 +17,43 @@ def test_server_player_relations():
     assert set(server.players) == set([player1, player2])
 
 
-def test_server_map_mode():
+def test_map_modes():
     map_ = factories.Map()
-    mode = factories.Mode()
-    map_mode = models.MapMode(map=map_, mode=mode)
+    mode1 = factories.Mode(maps=[map_])
+    mode2 = factories.Mode(maps=[map_])
 
-    server = factories.Server(map=map_, mode=mode)
-
-    db_session.add_all([server, map_mode])
+    db_session.add_all([mode1, mode2])
     db_session.commit()
 
-    assert server.map_id == map_.id
-    assert server.mode_id == mode.id
+    assert map_.modes == [mode1, mode2]
 
-    assert map_mode.map_id == map_.id
-    assert map_mode.mode_id == mode.id
+
+def test_mode_maps():
+    mode = factories.Mode()
+    map1 = factories.Map(modes=[mode])
+    map2 = factories.Map(modes=[mode])
+
+    db_session.add_all([map1, map2])
+    db_session.commit()
+
+    assert mode.maps == [map1, map2]
+
+
+def test_server_map_mode_servers():
+    map_ = factories.Map()
+    mode = factories.Mode(maps=[map_])
+
+    server1 = factories.Server(map=map_, mode=mode)
+    server2 = factories.Server(map=map_, mode=mode)
+
+    db_session.add_all([server2, server2])
+    db_session.commit()
+
+    assert server1.map_id == map_.id
+    assert server1.mode_id == mode.id
+
+    assert map_.modes == [mode]
+    assert map_.servers == [server1, server2]
+
+    assert mode.maps == [map_]
+    assert mode.servers == [server1, server2]
