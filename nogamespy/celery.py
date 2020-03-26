4139bbd1b08062e6297a6a9d2d7bc679952cac2c
Ondřej Garncarz <ondrej@garncarz.cz>
2020-03-26 12:18:55 +0100

    Celery: set result_expires programmatically.

--- nogamespy/celery.py
+++ nogamespy/celery.py
@@ -1,11 +1,14 @@
 from celery import Celery, signals
 
+from . import settings
 from .database import db_session
 
 
 app = Celery('nogamespy', include=['nogamespy.tasks'])
 
 app.config_from_object('nogamespy.settings')
+
+app.conf.result_expires = settings.CELERY_RESULT_EXPIRES
 
 
 @signals.task_postrun.connect
